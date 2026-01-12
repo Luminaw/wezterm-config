c3109d38deb381ac3dbdfd702d6bba432889d331
Luminaw <146209657+Luminaw@users.noreply.github.com>
2026-01-12 07:32:31 -0600

    feat: add encryption configuration for resurrect state management

--- config/resurrect.lua
+++ config/resurrect.lua
@@ -4,6 +4,13 @@ local resurrect = wezterm.plugin.require 'https://github.com/MLFlexer/resurrect.
 
 resurrect.state_manager.change_state_save_dir(wezterm.home_dir .. '/.wezterm-resurrect-states')
 
+resurrect.state_manager.set_encryption({
+   enable = true,
+   method = "rage",
+   private_key = wezterm.home_dir .. '/.keys/age',
+   public_key = "age140sjjdk934v5ly9r3g238zate8r9kjkdrgvz6gtm33e2g3mlh9tq93qfe7",
+})
+
 resurrect.state_manager.set_max_nlines(10000)
 
 resurrect.state_manager.periodic_save({
